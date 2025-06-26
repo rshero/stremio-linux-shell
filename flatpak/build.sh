a3e45def88167e4066e92804b8a488a14e6caf52
Tim <tymmesyde@gmail.com>
2025-06-26 11:44:05 +0200

    chore: move flatpak manifest to flatpak dir

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -5,5 +5,5 @@ cwd="flatpak"
 
 python3 $cwd/flatpak-builder-tools/cargo/flatpak-cargo-generator.py Cargo.lock -o $cwd/cargo-sources.json
 
-flatpak-builder --repo=$cwd/repo --force-clean $cwd/build $package_id.json
+flatpak-builder --repo=$cwd/repo --force-clean $cwd/build $cwd/$package_id.json
 flatpak build-bundle $cwd/repo $cwd/$package_id.flatpak $package_id
